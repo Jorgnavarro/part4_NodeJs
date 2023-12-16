68474b23a0e109b0a9a762e8038fecfed02e4ac5
Jorgnavarro <jorgluisnavarro@gmail.com>
2023-12-15 22:02:24 -0500

    added-set-databases-and-initial-testing

--- models/blog.js
+++ models/blog.js
@@ -1,10 +1,29 @@
 import mongoose from "mongoose";
 
 const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
+    title:{
+        type: String,
+        minlength: 5,
+        required: true
+    },
+    author: {
+        type: String,
+        minlength: 3,
+        required: true
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    likes: Number,
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
