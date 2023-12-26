d8e60c80407948a8a7d516ac6a15d445d7be5c44
Jorgnavarro <jorgluisnavarro@gmail.com>
2023-12-25 19:39:47 -0500

    suit-test-completed-exercises-4.8-4.12

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -37,7 +37,57 @@ test('The ID property must exist', async () => {
   })
 })
 
+test('A valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Adding a new blog from testing',
+    author: 'Luis Navarro',
+    url: 'http://jorgluisnavarro.com',
+    likes: 10
+  }
 
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsUpdate = await helper.blogsInDb()
+
+  expect(blogsUpdate).toHaveLength(helper.initialBlogs.length + 1)
+
+  const titles = blogsUpdate.map(blog => blog.title)
+
+  expect(titles).toContain('Adding a new blog from testing')
+})
+
+test('If the likes property is not there it should be zero', async () => {
+  const newBlog = {
+    title: 'Adding a new blog without likes property',
+    author: 'Luis Navarro',
+    url: 'http://jorgluisnavarro.com',
+  }
+
+  const blogCreated = await api
+    .post('/api/blogs')
+    .send(newBlog)
+
+  expect(blogCreated.body.likes).toBe(0)
+})
+
+test('A invalid blog can not be added', async () => {
+  const newBlog = {
+    author: 'Luis Navarro',
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsUpdate = await helper.blogsInDb()
+
+  expect(blogsUpdate).toHaveLength(helper.initialBlogs.length)
+})
 
 afterAll(() => {
   mongoose.connection.close()
