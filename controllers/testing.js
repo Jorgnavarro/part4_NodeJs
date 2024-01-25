e2a1e5aa76120adf256290f9be634d3d5c3f295c
Jorgnavarro <jorgluisnavarro@gmail.com>
2024-01-25 10:43:03 -0500

    last-changes-with-eslint

--- controllers/testing.js
+++ controllers/testing.js
@@ -4,11 +4,10 @@ import User from '../models/user.js'
 
 const testingRouter = Router()
 
-
 testingRouter.post('/reset', async (req, res) => {
-    await Blog.deleteMany({})
-    await User.deleteMany({})
-    res.status(204).end()
+  await Blog.deleteMany({})
+  await User.deleteMany({})
+  res.status(204).end()
 })
 
 export default testingRouter
